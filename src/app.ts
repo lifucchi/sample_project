e7bd71b0fd31bbca10f4fa866ed31b62121a2d8d
Rifka Annisa <Geminigirl124@gmail.com>
2021-12-31 10:22:44 +0700

    clean

--- src/app.ts
+++ src/app.ts
@@ -22,21 +22,7 @@ app.use('/albums', albumRoutes );
 
 // ================================================================
 
-
-
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({message: err.message});
 })
 app.listen(3000);
-// sequelize
-//   .sync()
-// // .sync({alter: true})
-// //   .sync({force: true})
-//   .then(result => {
-//     app.listen(3000);
-//   })
-//   .catch( err => {
-//     console.log(err);
-//     process.exit(1);
-//   });
-
